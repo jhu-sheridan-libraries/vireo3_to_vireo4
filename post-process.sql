3a906c14773b0173fee34d118c43e14ee9e63fef
jrmartino <jrm@jhu.edu>
2025-07-02 06:46:25 -0400

    remove stray embargo definition

--- post-process.sql
+++ post-process.sql
@@ -1,2 +1,3 @@
 DELETE FROM deposit_location WHERE id>0;
 ALTER SEQUENCE deposit_location_id_seq RESTART WITH 1;
+DELETE FROM embargo WHERE guarantor = 'DEFAULT' AND name = 'None' AND description = 'The work will be published after approval.';
